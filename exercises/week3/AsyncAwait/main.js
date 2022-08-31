8bf42b57fdfb23aca1d48cd8be236db2fb559831
orya <orya.sp@gmail.com>
2022-08-31 17:12:40 +0300

    Week 3 - Async Await

---
+++ exercises/week3/AsyncAwait/main.js
@@ -0,0 +1,22 @@
+
+// sc 1
+// How would you rewrite the below recipe API call with async/await?
+const fetchRecipes = function (ingredient) {
+    $.get(`/recipe/${ingredient}`, function(recipe){
+      console.log(recipe)
+    });
+}
+
+
+// ans = 
+const fetchRecipes = async function (surname) {
+    let data = await $.get(`/recipe/${ingredient}`)
+    console.log(data)
+}
+
+
+
+
+// sc 2
+
+
