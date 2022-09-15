f2c37eba989d793bea5a141a2d7faad7912ac12a
orya <orya.sp@gmail.com>
2022-09-15 18:59:58 +0300

    Week 5 - Python Pytest - exercise_1_test.py

---
+++ exercises/week5/PythonIntermediate/tests/exercise_1_test.py
@@ -0,0 +1,6 @@
+
+def test_mul():
+    assert 2 * 3 == 6, "Test failed! Wrong total when multiplying two regular numbers"
+    
+def test_mul_with_zero():
+    assert 2 * 0 == 0, "Test failed! Wrong total when multiplying two regular numbers"
